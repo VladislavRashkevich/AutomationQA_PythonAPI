b593ae132d44d18b9d501e9b0cdda3d4d5aa8375
Lasloflo <qrash1992@gmail.com>
2023-02-26 18:51:25 +0300

    add test: check details not auth user

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -1,3 +1,5 @@
+import random
+
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 from lib.my_requests import MyRequests
@@ -37,3 +39,24 @@ class TestUserGet(BaseCase):
         # Assertions.assert_json_has_key(response2, "firstName")
         # Assertions.assert_json_has_key(response2, "lastName")
 
+    def test_get_only_username_not_auth_auth(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+
+        response_login = MyRequests.post('/user/login', data=data)
+        auth_sid = self.get_cookie(response_login, "auth_sid")
+        token = self.get_header(response_login, "x-csrf-token")
+        user_id = self.get_json_value(response_login, "user_id")
+
+        cookies = {"auth_sid": auth_sid}
+        headers = {"x-csrf-token": token}
+
+        response_get_details = MyRequests.get(f"/user/1000", headers=headers, cookies=cookies)
+
+        expected_fields = ["email", "firstName", "lastName"]
+
+        Assertions.assert_code_status(response_get_details, 200)
+        Assertions.assert_json_has_key(response_get_details, 'username')
+        Assertions.assert_json_has_not_keys(response_get_details, expected_fields)
